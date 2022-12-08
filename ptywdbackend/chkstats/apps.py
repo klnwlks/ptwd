754c784c4c8f79042f8392044c448a94317cf93e
klnwlks <95198127+klnwlks@users.noreply.github.com>
2022-12-08 18:19:29 +0800

    stage deleted files

--- ptywdbackend/chkstats/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ChkstatsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'chkstats'
--- ptywdbackend/chkstats/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-# Create your models here.
-class client(models.Model):
-    identifier = models.CharField(default='', max_length=100)
-    due = models.DateTimeField()
-    paid = models.BooleanField(default=True)
